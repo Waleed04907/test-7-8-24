756436567b013c8df63623da12158867b77b2ada
waleed-saddique <wbs90704@gmail.com>
2024-08-07 21:22:18 +0500

    test file for varialbes

---
+++ today.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("test today");
+console.log("this is my test file");
+console.log("this is my test file for today class");
+console.log("this is my test file, i wull make good practic today");
+let mesg = "hi world";
+console.log(mesg);
+const author = "waleed khan";
+console.log(author);
